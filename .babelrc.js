19e5615e735ef4f0fa132a09b66a90eced2caa18
Umidbek Karimov <uma.karimov@gmail.com>
2019-12-10 22:12:51 +0500

    chore(.babelrc.js): Remove dead code.

--- .babelrc.js
+++ .babelrc.js
@@ -1,25 +1,5 @@
 'use strict';
 
-// module.exports = api => {
-//   const isTest = api.env("test");
-//
-//   api.cache(() => JSON.stringify({ isTest }));
-//
-//   const presets = ["@babel/preset-typescript"];
-//   const plugins = [
-//     ["@babel/plugin-proposal-class-properties", { loose: true }],
-//   ];
-//
-//   if (isTest) {
-//     presets.unshift([
-//       "@babel/preset-env",
-//       { loose: true, modules: "commonjs", targets: { node: true } },
-//     ]);
-//   }
-//
-//   return { presets, plugins };
-// };
-
 const isTest = process.env.NODE_ENV === 'test';
 
 module.exports = {
