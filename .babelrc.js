eb3430b89ead6f9192f9cb7f07baaddc9571f8f3
Umidbek Karimov <4734297+umidbekk@users.noreply.github.com>
2020-04-15 15:59:23 +0500

    chore: Use `@superdispatch/babel-preset`.

--- .babelrc.js
+++ .babelrc.js
@@ -1,16 +1,20 @@
 'use strict';
 
-const isTest = process.env.NODE_ENV === 'test';
+module.exports = (api) => {
+  const isTest = api.env('test');
 
-module.exports = {
-  compact: false,
-  presets: [
-    isTest && '@babel/preset-typescript',
-    isTest && ['@babel/preset-env', { targets: { node: true } }],
-  ].filter(Boolean),
-  plugins: [
-    ['@babel/plugin-proposal-class-properties', { loose: true }],
-    ['@babel/plugin-proposal-optional-chaining', { loose: true }],
-    ['@babel/plugin-proposal-nullish-coalescing-operator', { loose: true }],
-  ],
+  api.cache(() => JSON.stringify({ isTest }));
+
+  return {
+    presets: [
+      [
+        '@superdispatch/babel-preset',
+        {
+          loose: true,
+          targets: 'esmodules',
+          optimize: { runtime: false, devExpressions: true },
+        },
+      ],
+    ],
+  };
 };
