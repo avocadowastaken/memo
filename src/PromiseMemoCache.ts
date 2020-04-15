7854171515e417ba4d877ef3a201cb9dab4ea655
Umidbek Karimov <4734297+umidbekk@users.noreply.github.com>
2020-04-15 15:49:09 +0500

    chore: Bump dependencies and restore `yarn.lock`.

--- src/PromiseMemoCache.ts
+++ src/PromiseMemoCache.ts
@@ -7,7 +7,7 @@ export class PromiseMemoCache<TKey, TValue> extends MemoCache<
   prime(key: TKey, value: TValue | Promise<TValue>): this {
     return super.prime(
       key,
-      Promise.resolve(value).catch(error => {
+      Promise.resolve(value).catch((error) => {
         this.clear(key);
 
         return Promise.reject(error);
